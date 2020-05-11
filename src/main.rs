f185acffacc738e3f77a034429a5865475612bb6
Audran Doublet <audran.doublet@epita.fr>
2020-05-11 13:59:31 +0200

    add very simple main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use nalgebra::Vector3;
+
+use world::{World, ChunkListener};
+
+pub struct MyChunkListener {
+    pub loaded_chunks: Vec<(i64, i64)>,
+    pub unloaded_chunks: Vec<(i64, i64)>,
+}
+
+impl MyChunkListener {
+    pub fn new() -> MyChunkListener {
+        MyChunkListener {
+            loaded_chunks: Vec::new(),
+            unloaded_chunks: Vec::new(),
+        }
+    }
+
+    pub fn update_renderer(&mut self) {
+        //FIXME
+        self.clear();
+    }
+
+    pub fn clear(&mut self) {
+        self.loaded_chunks.clear();
+        self.unloaded_chunks.clear();
+    }
+}
+
+impl ChunkListener for MyChunkListener {
+    fn chunk_load(&mut self, x: i64, y: i64) {
+        self.loaded_chunks.push((x, y));
+    }
+
+    fn chunk_unload(&mut self, x: i64, y: i64) {
+        self.unloaded_chunks.push((x, y));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut listener = MyChunkListener::new();
+
+    let mut world = World::new();
+    let mut player = world.create_player(&mut listener);
+
+    // FIXME main loop
+    player.update(&mut world, &mut listener, Vector3::z(), Vector3::x(), Vec::new(), 0.1);
+    listener.update_renderer();
 }
